299eac43aa929f92a3aaa4b0a2c77dc28b10c073
Matheus <mthsilva1998@gmail.com>
2024-10-03 23:01:47 -0300

    modificado o os nomes dos packs e cruado uma class User

--- src/main/java/io/github/mthsilva98/App.java
+++ src/main/java/io/github/mthsilva98/App.java
@@ -1,4 +1,4 @@
-package org.example;
+package io.github.mthsilva98;
 
 /**
  * Hello world!
--- src/test/java/io/github/mthsilva98/AppTest.java
+++ src/test/java/io/github/mthsilva98/AppTest.java
@@ -1,4 +1,4 @@
-package org.example;
+package io.github.mthsilva98;
 
 import junit.framework.Test;
 import junit.framework.TestCase;
