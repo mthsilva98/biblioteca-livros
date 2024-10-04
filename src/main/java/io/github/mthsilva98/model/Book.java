25ebab6eb972669cab4ee307d5eaa972a1e73e1a
Matheus <mthsilva1998@gmail.com>
2024-10-04 12:56:38 -0300

    Feito os atributos, sets, gats, e tostring da class Book

---
+++ src/main/java/io/github/mthsilva98/model/Book.java
@@ -0,0 +1,43 @@
+package io.github.mthsilva98.model;
+
+public class Book {
+    private String title;
+    private String author;
+    private boolean available;
+
+    public Book(String title, String author) {
+        this.title = title;
+        this.author = author;
+        this.available = true;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
+
+    @Override
+    public String toString() {
+
+        return "Livro: " + title + "\n Autor: " + author + "\n Disponivel: " + available;
+    }
+}
