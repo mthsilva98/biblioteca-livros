d47cd70ffb893b9ab3ee2a684630c630a6569186
Matheus <mthsilva1998@gmail.com>
2024-10-19 22:12:59 -0300

    adicionado o metodo salvar livro na class LivroDAO

--- src/main/java/oi/github/mthsilva98/dao/LivroDAO.java
+++ src/main/java/oi/github/mthsilva98/dao/LivroDAO.java
@@ -1,4 +1,28 @@
 package oi.github.mthsilva98.dao;
 
+import oi.github.mthsilva98.model.Livro;
+import oi.github.mthsilva98.util.DatabaseConnection;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class LivroDAO {
+
+    public void salvar(Livro livro) {
+        String sql = "INSERT INTO livros (titulo, autor, genero) VALUES (?, ?, ?)";
+
+        try (Connection conn = DatabaseConnection.getConexao();
+             PreparedStatement stmt = conn.prepareStatement(sql)){
+
+            stmt.setString(1, livro.getTitulo());
+            stmt.setString(2, livro.getAutor());
+            stmt.setString(3, livro.getGenero());
+            stmt.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    // Outros métodos CRUD (atualizar, excluir, buscar) podem ser implementados aqui.
 }
