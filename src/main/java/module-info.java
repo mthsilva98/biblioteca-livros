0b5c3dc0376138ad8a741d5db343472bcee82916
Matheus <mthsilva1998@gmail.com>
2024-10-19 21:07:53 -0300

    Feito todo o codigo da class databaseConnection

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module org.example.bibliotecalivros {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
 
     opens oi.github.mthsilva98 to javafx.fxml;
--- src/main/java/oi/github/mthsilva98/util/DatabaseConnection.java
+++ src/main/java/oi/github/mthsilva98/util/DatabaseConnection.java
@@ -1,4 +1,30 @@
 package oi.github.mthsilva98.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DatabaseConnection {
+
+    private static final String url = "jdbc:mysql://localhost:3306/biblioteca_livros";
+    private static final String user = "root";
+    private static final String password = "123456Ma";
+
+    private static Connection conn;
+
+    public static Connection getConexao() {
+
+        try {
+            if(conn == null) {
+                conn = DriverManager.getConnection(url, user, password);
+                return conn;
+            }else {
+                return conn;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
 }
