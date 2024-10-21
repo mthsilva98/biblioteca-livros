0c34744f8591b9b3b4282d51dced794fd7063d40
Matheus <mthsilva1998@gmail.com>
2024-10-21 15:41:07 -0300

    Arrumado os metodos do livro

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,9 +3,11 @@ module org.example.bibliotecalivros {
     requires javafx.fxml;
     requires java.sql;
 
-
+    // Abre o pacote `oi.github.mthsilva98.model` para o módulo `javafx.base`
+    opens oi.github.mthsilva98.model to javafx.base;
     opens oi.github.mthsilva98 to javafx.fxml;
+    opens oi.github.mthsilva98.controller to javafx.fxml;
+
     exports oi.github.mthsilva98;
     exports oi.github.mthsilva98.controller;
-    opens oi.github.mthsilva98.controller to javafx.fxml;
 }
